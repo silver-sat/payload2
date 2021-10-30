29a5df61dd81759c79e3724c2cad4a7308c26430
Nathan Edwards <edwardsnj@users.noreply.github.com>
2021-10-30 16:26:36 -0400

    Update files.py

--- files.py
+++ files.py
@@ -5,13 +5,7 @@ import glob
 def photo_filename():
     return filename
 
-def create_file(filename):
-    h = open(filename,'w')
-    h.close()
-
 def most_recent_photo():
-    for f in glob.glob('photo_*.jpg'):
-        
     return filename
 
 def remove_photo(filename):
